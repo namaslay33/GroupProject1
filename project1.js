03583f2347b21b600da7bade24d09a2ebbd64fe8
Preston Neal <prestonneal@Prestons-MBP.localdomain>
2018-08-03 12:47:38 -0500

    updated maps

--- project1.js
+++ project1.js
@@ -26,9 +26,11 @@ $(function(){
     function myFunction(response) {
         var $sidebar = $("#sidebar");
         var myLatLng = [];
+        var myContents = [];
+
         var $xmlResponse = $(response);
         var counter = 0;
-        var $map = $("#map");
+        console.log(response);
         $xmlResponse.find('region').each(function(){
             // console.log($(this));
             var $region = $(this);
@@ -40,11 +42,15 @@ $(function(){
             var longitude = $region.find('longitude').text();
 
             var position = {};
-            
             position["lat"] = $region.find('latitude').text();
             position["lng"] = $region.find('longitude').text();
-            // position["name"] = $region.find('name').text();
             myLatLng.push(position);
+
+            var contents = {};
+            contents["name"] = $region.find('name').text();
+            contents["url"] = $region.find('url').text();
+            myContents.push(contents);
+            
 
             if (counter > 0 && counter < 50) {
             $sidebar.append(name + "<br>" + "Median price: $" + medianValue + "<br><br>");
@@ -53,24 +59,80 @@ $(function(){
             }
             counter++;
 
+
+                
+              
     //outside of loop
         
 })
-        function initMap() {
+
+    }
+    
+});
+
+
+function initMap() {
             
-            var patharray = [];
-            var map;
-            for (var i = 1; i<myLatLng.length; i++){
-                patharray.push(new google.maps.LatLng(myLatLng[i][0], myLatLng[i][1]));
-            }
-            var tourplan = new google.maps.Polyline({
-                path: patharray,
-                strokeColor: "#000FF",
-                strokeOpacity: 0.6,
-                strokeWeight : 2,
-            });
-            tourplan.setMap(map)
-            }
+    // var patharray = [];
+    // Map options
+    var options = {
+        zoom : 8,
+        center : {lat: 47.614848, lng: -122.33607}
+    }
+    
+    // New map
+    var map = new 
+    google.maps.Map(document.getElementById('map'), options);
+
+    // Add marker function
+    function addMarker(props){
+    var marker = new google.maps.Marker({
+        position:props.coords,
+        map: map
+    });
+
+        // Check content
+        if(props.content){
+          // Info window
+          var infoWindow = new
+          google.maps.InfoWindow({
+          content: props.content
+          });
+
+          marker.addListener('click', function(){
+            infoWindow.open(map, marker);
+          })
+        }
+      }
+      
+}
+ // // Add marker
+            // var marker = new 
+            // google.maps.Marker({
+            //   position:{lat: 42.3601,lng: -71.0589},
+            //   map: map
+            // });
+
+            // // Info window
+            // var infoWindow = new
+            // google.maps.InfoWindow({
+            //   content: '<h1>hi</h1>'
+            // });
+            // marker.addListener('click', function(){
+            //   infoWindow.open(map, marker);
+            // })
+
+            // for (var i = 1; i<myLatLng.length; i++){
+            //     patharray.push(new google.maps.LatLng(myLatLng[i][0], myLatLng[i][1]));
+            // }
+            // var tourplan = new google.maps.Polyline({
+            //     path: patharray,
+            //     strokeColor: "#000FF",
+            //     strokeOpacity: 0.6,
+            //     strokeWeight : 2,
+            // });
+            // tourplan.setMap(map)
+            // }
         //     var map = new google.maps.Map(document.getElementById('map'), {
         //       zoom: 4,
         //       center: (myLatLng[0][0], myLatLng[0][1]
@@ -82,7 +144,3 @@ $(function(){
         //       title: myLatLng[i][2],
         // }
         //     });
-        //   }
-      }
-    
-});
