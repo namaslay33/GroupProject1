5da130592ae132da30aa19e6dc649c9592175a8c
Namaslay33 <skylar.hunt@utexas.edu>
2018-08-06 21:50:53 -0500

    Changes to event/survey

--- events.js
+++ events.js
@@ -11,38 +11,97 @@ $(function(){
        
 
         $.get("https://app.ticketmaster.com/discovery/v2/events.json?" + "&city=" + city + "&state=" + state + "&classification=" + event + "&apikey=" + key)
-        .done(function(response){
-            console.log(response);
-            console.log(response._embedded.events[0]);
-            // ticketmaster(response);
-            console.log(event);
+        .done(function(info){
+            console.log(info);
+            // console.log(response._embedded.events[0].name);
+            // ticketMaster(response); 
+            // ticketMasterError(response);
+
+            for(item in info){
+               
+                $("<td>").html(info._embedded.events[0].name)
+                // $("<td>").html(info._embedded.events[1].url)
+                // $("<td>").html(info._embedded.events[1].url)
+                .appendTo($("#eventInfo"))
+            }
+           
         })
     })
 
-    .fail(function(error){
 
-    })
+    // .fail(function(error){
+    //     console.log(error);
+    // })
+ 
+    // function ticketMaster (response) {
+
+    //     var events = response._embedded.events;
+
+    //     var objectArray = [];
+
+    //     //Use for in because my api info was an object not an array
+    //     for (var e in events) {
+
+    //         var eventsObj = events[e];
+
+    //             try {
+
+                
+    //             var info = [eventsObj.images[0].url, eventsObj.promoter.name, eventsObj.name, eventsObj.dates.start.localDate, eventsObj.dates.start.localTime, eventsObj._embedded.venues[0].name, eventsObj._embedded.venues[0].address.line1, eventsObj._embedded.venues[0].city.name, eventsObj._embedded.venues[0].state.name, eventsObj._embedded.venues[0].postalCode, eventsObj.url];
+    //             objectArray.push(info);
+            
+               
+    //     // $eventInfo.append("<br>" + info[0] + "</br>" + "<br>" + info[1] + "'s bringing... </br><br>Name: " + info[2] + "</br><br>Date: " + info[3] + " Time: " + info[4] + "</br><br>Venue: " + info[5] + "</br><br>Address: " + info[6] + "</br><br>" + info[7] + ", " + info[8] + " " + info[9] + "</br><br>Website: " + info[10] + "</br>")
+    //             // console.log("printed here: " + info);
+    //             }
+    //             catch {}
+    //         //console.log(e + ": " + eventsObj.images[0].url);
+
+    //         // try{
+    //         //     console.log(e + ": " +eventsObj.promoter.name);
+    //         // }
+    //         // catch{"error at index: " + e}
+    //         // console.log(eventsObj);
+            
+            
+    //     }
+    //     console.log(objectArray);
+
+    //     for(let event in objectArray){
+    //         for(item in event){
+    //             console.log(item)
+    //         }
+    //     }
+
+        
+            
+            // console.log(events[info].images[0].url)
+        
+    // }
+        // for loops only work on arrays
+        // for (i=0; i < events.length(); i++) {
+        //     var eventsArr = events[i];
+        //     info = [eventsArr.images[0].url, eventsArr.promoter.name, eventsArr.name, eventsArr.dates.start.localDate, eventsArr.dates.start.localTime, eventsArr.venues[0].name, eventsArr.venues[0].address.line1, eventsArr.venues[0].city.name, eventsArr.venues[0].state.name, eventsArr.venues[0].postalCode, eventsArr.url]
+        // }
+    //     var $eventInfo = $("#eventInfo");
+    //     $eventInfo.append("<br>" + info[0] + "</br>" + "<br>" + info[1] + "'s bringing... </br><br>Name: " + info[2] + "</br><br>Date: " + info[3] + " Time: " + info[4] + "</br><br>Venue: " + info[5] + "</br><br>Address: " + info[6] + "</br><br>" + info[7] + ", " + info[8] + " " + info[9] + "</br><br>Website: " + info[10] + "</br>")
+    // }
+
+    // console.log(eventsArr);
+
+//     function ticketMasterError (respone) {
+//         var $eventInfo = $("#eventInfo");
+// 		$eventInfo.addClass("hidden");
+//     }
+    
     
 });
 
-function ticketMaster (response) {
-    var $eventInfo = $("#eventInfo");
 
-    
-    // embedded= response.getElementsByTagName("_embedded");
+
+// embedded= response.getElementsByTagName("_embedded");
     // array = info[0];
     // events = array.getElementsByTagName("events");
-
-    var events = response._embedded.events[0];
-    console.log(response._embedded.events[0]);
-    
-    for (i=0; i < array.length(); i++) {
-        var arrayIndex = events[i];
-        info = [arrayIndex[6].getElementsByTagName("url"), arrayIndex[10].getElementByTagName("name"), arrayIndex.getElementsByTagName("name"), arrayIndex[8][0].getElementsByTagName("localDate"), arrayIndex[8][0].getElementsByTagName("localTime"), arrayIndex[16][0].getElementsByTagName("name"), arrayIndex[16][0][12].getElementsByTagName("line1"), arrayIndex[16][0][9].getElementsByTagName("name"), arrayIndex[16][0][10].getElementsByTagName("name"), arrayIndex[16][0].getElementsByTagName("postalCode"), arrayIndex.getElementsByTagName("url")]
-    }
-
-    $eventInfo.append("<br>" + info[0] + "</br>" + "<br>" + info[1] + "'s bringing... </br><br>Name: " + info[2] + "</br><br>Date: " + info[3] + " Time: " + info[4] + "</br><br>Venue: " + info[5] + "</br><br>Address: " + info[6] + "</br><br>" + info[7] + ", " + info[8] + " " + info[9] + "</br><br>Website: " + info[10] + "</br>")
-}
 
 // array.forEach(element => {
     
