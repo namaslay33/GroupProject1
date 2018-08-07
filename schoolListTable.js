b6fe1a1d8840e1bd14a7f779f85256dd7af089c1
hussein.a.khorshed <hussein.a.khorshed@gmail.com>
2018-08-07 10:24:40 -0500

    Table Draft

--- schoolListTable.js
+++ schoolListTable.js
@@ -13,9 +13,10 @@ $.getJSON('https://code.org/schools.json' , function(data) {
           state = data.schools[i].state,
           zip = data.schools[i].zip;
 
+          
+
     var tr = $('<tr/>');
-        tr.append("<td class='schoolName'>" + name + "</td>");
-        tr.append("<td class='schoolWebsite'>" + website + "</td>");
+        tr.append("<td class='schoolName'>" +"<a   target='_blank' href='" + website + "'>" + name +  "</a>"+ "</td>");
         tr.append("<td class='schoolStreet'>" + street + "</td>");
         tr.append("<td class='schoolCity'>" + city + "</td>");
         tr.append("<td class='schoolState'>" + state + "</td>");
@@ -23,26 +24,27 @@ $.getJSON('https://code.org/schools.json' , function(data) {
         $('#myTable').append(tr);
 
 
-    function searchFunction() {
-        var input, filter, table, tr, td, i;
-        input = document.getElementById("myInput");
-        filter = input.value.toUpperCase();
-        table = document.getElementById("myTable");
-        tr = table.getElementsByTagName("tr");
-        for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[0];
-            if (td) {
-            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-            }       
-        }
     }
 
-    }
 });
+
+function searchFunction() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+        } else {
+            tr[i].style.display = "none";
+        }
+        }       
+    }
+}
 
 
         
