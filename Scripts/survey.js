341b331c593f33a48f2a7100f29e558119d1405e
Preston Neal <prestonneal@Prestons-MBP.localdomain>
2018-08-07 19:37:30 -0500

    last one tonight

--- Scripts/survey.js
+++ Scripts/survey.js
@@ -1,27 +1,8 @@
-$(function() {
-    $('.error').hide();
-    $(".submitBtn").click(function() {
-      // validate and process form here
-      
-      $('.error').hide();
-  	  var name = $("input#name").val();
-  		if (name == "") {
-        $("label#name_error").show();
-        $("input#name").focus();
-        return false;
-      }
-  		var email = $("input#email").val();
-  		if (email == "") {
-        $("label#email_error").show();
-        $("input#email").focus();
-        return false;
-      }
-  		var phone = $("input#phone").val();
-  		if (phone == "") {
-        $("label#phone_error").show();
-        $("input#phone").focus();
-        return false;
-      }
-      
-    });
-  });
+//links to seperate contact pages
+
+// $(".submitBtn").submit(function(e) {
+//   e.preventDefault();
+//     //prevent form from being submitted
+//     //ajax to post form data to create.php
+// });
+
